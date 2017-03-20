42574b81bcfee2c261deb8fd28f83f1b51b4fed2
Sub Admin <subadmin@Hoais-MBP.net.fpt>
2017-03-21 01:14:27 +0700

    add refresh control

--- apps/movieList.js
+++ apps/movieList.js
@@ -8,7 +8,8 @@ import {
     TouchableOpacity,
     ListView,
     Navigator,
-    TextInput
+    TextInput,
+    RefreshControl
 } from 'react-native';
 
 import Movie from './movie';
@@ -20,6 +21,8 @@ export default class MovieList extends Component {
         this.state = {
             movieList: [],
             loading: true,
+            refreshing: false,
+            page: 1,
             moviesDataSource: new ListView.DataSource({
                 rowHasChanged: (row1, row2) => row1 !== row2,
             })
@@ -49,10 +52,28 @@ export default class MovieList extends Component {
                             </TouchableOpacity>
 
                     }
+                    refreshControl={
+                        <RefreshControl
+                            refreshing={this.state.refreshing}
+                            onRefresh={this._onRefresh.bind(this)}
+                        />
+                    }
                 />
             </View>
         )
     }
+    _onRefresh() {
+        let page = this.state.page + 1 ; 
+        this.setState({ refreshing: true, page: page });
+
+        this.getMoviesFromApiAsync(page).then((movies) => {
+            console.log("refesh data " + this.state.page);
+            console.log(movies);
+            this.setState({ refreshing: false });
+            this.setMovieList(movies , true ) ;
+        });
+    }
+
     filterMovies(name) {
         let keyword = name.text.toLowerCase();
         let rows = [];
@@ -70,22 +91,31 @@ export default class MovieList extends Component {
     }
 
     componentDidMount() {
-        this.getMoviesFromApiAsync().then(movies => {
-            this.setState({ loading: false, movieList: movies });
-            let ds = new ListView.DataSource({ rowHasChanged: () => { (r1, r2) => r1 !== r2 } });
-            this.setState({ moviesDataSource: ds.cloneWithRows(this.state.movieList) });
+        this.getMoviesFromApiAsync(1).then(movies => {
+            this.setMovieList(movies, false);
 
         })
     }
+    setMovieList(movies, isAdded) {
+        if (isAdded) {
+            this.setState({ loading: false, movieList: movies.concat (this.state.movieList)});
 
-    getMoviesFromApiAsync() {
-        console.log("type: ", this.props.type);
+        } else {
+            this.setState({ loading: false, movieList: movies });
+        }
+        console.log(this.state.movieList);
+        let ds = new ListView.DataSource({ rowHasChanged: () => { (r1, r2) => r1 !== r2 } });
+        this.setState({ moviesDataSource: ds.cloneWithRows(this.state.movieList) });
+
+    }
+
+    getMoviesFromApiAsync(page) {
         let type = this.props.type;
         let url = '';
         if (type == 'NOW_PLAYING') {
-            url = 'https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed';
+            url = 'https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed&page=' + page;
         } else {
-            url = 'https://api.themoviedb.org/3/movie/top_rated?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed';
+            url = 'https://api.themoviedb.org/3/movie/top_rated?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed&page=' + page;
         }
         return fetch(url)
             .then((response) => response.json())
