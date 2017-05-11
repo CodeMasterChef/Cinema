7b2af12a7f9d99554b460616cf581b8aabfe87af
NinhLe <levanninh0101@gmail.com>
2017-05-11 07:49:05 +0700

    try to add splashcreen

--- android/app/src/main/java/com/cinema/MainApplication.java
+++ android/app/src/main/java/com/cinema/MainApplication.java
@@ -3,6 +3,7 @@ package com.cinema;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativecomponent.splashscreen.RCTSplashScreenPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,7 +25,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new VectorIconsPackage()
+            new RCTSplashScreenPackage(),
+            new VectorIconsPackage(),
+            new RCTSplashScreenPackage()    
       );
     }
   };
